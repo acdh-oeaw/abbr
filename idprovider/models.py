3dce53671d22b2bd1943fcc88ae80a708b19f2c2
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2018-04-26 12:33:36 +0200

    minor

--- idprovider/models.py
+++ idprovider/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class IdProvider(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.id
